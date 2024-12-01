4df54cdaa55aa9a6763295c956374f2afeab8135
drzbida <55928036+drzbida@users.noreply.github.com>
2024-12-01 23:21:14 +0200

    feat/day1-2024

--- src/AdventOfCode.Solutions/Year2024/Day1.cs
+++ src/AdventOfCode.Solutions/Year2024/Day1.cs
@@ -17,11 +17,37 @@ public class Day1 : AdventOfCodeSolution
 
     protected override string PartOne(string[] lines)
     {
-        return "1";
+        var (left, right) = ParseInput(lines);
+        left.Sort();
+        right.Sort();
+
+        return Enumerable
+            .Zip(left, right)
+            .Select(x => Math.Abs(x.First - x.Second))
+            .Sum()
+            .ToString();
     }
 
     protected override string PartTwo(string[] lines)
     {
-        throw new NotImplementedException();
+        var (left, right) = ParseInput(lines);
+        var rightMap = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+        return left.Select(x => x * rightMap.GetValueOrDefault(x, 0)).Sum().ToString();
+    }
+
+    private static (List<int> left, List<int> right) ParseInput(string[] lines)
+    {
+        var left = new List<int>();
+        var right = new List<int>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(parts[0]));
+            right.Add(int.Parse(parts[1]));
+        }
+
+        return (left, right);
     }
 }
