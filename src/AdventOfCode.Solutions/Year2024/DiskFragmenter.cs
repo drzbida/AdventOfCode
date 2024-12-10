eeef43664df54f40a028b080a82d0b27e471fb22
drzbida <55928036+drzbida@users.noreply.github.com>
2024-12-10 02:04:35 +0200

    feat/day9-init

---
+++ src/AdventOfCode.Solutions/Year2024/DiskFragmenter.cs
@@ -0,0 +1,23 @@
+using AdventOfCode.Core.Common;
+using AdventOfCode.Core.Utils;
+
+namespace AdventOfCode.Solutions.Year2024;
+
+[AdventOfCode(2024, 9)]
+public class DiskFragmenter : AdventOfCodeSolution<int>
+{
+    protected override string Test =>
+        """
+            2333133121414131402
+            """;
+
+    protected override int PartOne(string[] lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override int PartTwo(string[] lines)
+    {
+        throw new NotImplementedException();
+    }
+}
