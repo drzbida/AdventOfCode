60014f5d4a822923bf53f1622b4e9fe5ed5cb352
drzbida <55928036+drzbida@users.noreply.github.com>
2024-12-16 02:38:09 +0200

    feat/day15-2024

--- src/AdventOfCode.Solutions/Year2024/WarehouseWoes.cs
+++ src/AdventOfCode.Solutions/Year2024/WarehouseWoes.cs
@@ -31,112 +31,124 @@ public partial class WarehouseWoes : AdventOfCodeSolution<long>
             v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
             """;
 
-    private abstract class GameObject
+    // VERY overkill for what it was needed, did it for fun
+    // as i needed to refactor the code to support the second part anyway
+    // supports infinite width and height for the objects
+
+    private abstract class GameObject(Vector2Int origin, int width, int height, char[] display)
     {
-        public int StartX { get; set; }
-        public int StartY { get; set; }
-        public int EndX { get; set; }
-        public int EndY { get; set; }
-        public char Type { get; set; }
+        public Vector2Int Origin { get; set; } = origin;
+        public int Width { get; set; } = width;
+        public int Height { get; set; } = height;
+        public char[] Display { get; set; } = display;
         public abstract bool CanPush(Vector2Int dir, GameObject[,] map);
         public abstract void Push(Vector2Int dir, GameObject[,] map);
+
+        public override string ToString() => string.Join("", Display);
     }
 
-    private class Wall : GameObject
+    private abstract class Immovable(Vector2Int origin, int width, int height, char[] display)
+        : GameObject(origin, width, height, display)
     {
-        public Wall(int x, int y)
-        {
-            StartX = x;
-            StartY = y;
-            EndX = x;
-            EndY = y;
-            Type = '#';
-        }
-
         public override bool CanPush(Vector2Int dir, GameObject[,] map) => false;
 
         public override void Push(Vector2Int dir, GameObject[,] map) { }
     }
 
-    private class Empty : GameObject
+    private abstract class Pushable(Vector2Int origin, int width, int height, char[] display)
+        : GameObject(origin, width, height, display)
     {
-        public Empty(int x, int y)
+        private List<Vector2Int> GetObjectEdge(Vector2Int dir)
         {
-            StartX = x;
-            StartY = y;
-            EndX = x;
-            EndY = y;
-            Type = '.';
+            var posToCheck = new List<Vector2Int>();
+
+            if (dir == Vector2Int.Up)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    posToCheck.Add(Origin + new Vector2Int(x, 0));
+                }
+            }
+            else if (dir == Vector2Int.Down)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    posToCheck.Add(Origin + new Vector2Int(x, Height - 1));
+                }
+            }
+            else if (dir == Vector2Int.Left)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    posToCheck.Add(Origin + new Vector2Int(0, y));
+                }
+            }
+            else if (dir == Vector2Int.Right)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    posToCheck.Add(Origin + new Vector2Int(Width - 1, y));
+                }
+            }
+
+            return posToCheck;
         }
 
+        public override bool CanPush(Vector2Int dir, GameObject[,] map)
+        {
+            var posToCheck = GetObjectEdge(dir);
+            return posToCheck.All(pos =>
+            {
+                var nextPos = pos + dir;
+                var next = map[nextPos.Y, nextPos.X];
+                return next.CanPush(dir, map);
+            });
+        }
+
+        public override void Push(Vector2Int dir, GameObject[,] map)
+        {
+            var posToCheck = GetObjectEdge(dir);
+            foreach (var pos in posToCheck)
+            {
+                var nextPos = pos + dir;
+                var next = map[nextPos.Y, nextPos.X];
+                next.Push(dir, map);
+            }
+
+            for (var y = Origin.Y; y < Origin.Y + Height; y++)
+            {
+                for (var x = Origin.X; x < Origin.X + Width; x++)
+                {
+                    map[y, x] = new Empty(new Vector2Int(x, y));
+                }
+            }
+
+            Origin += dir;
+
+            for (var y = Origin.Y; y < Origin.Y + Height; y++)
+            {
+                for (var x = Origin.X; x < Origin.X + Width; x++)
+                {
+                    map[y, x] = this;
+                }
+            }
+        }
+    }
+
+    private class Wall(Vector2Int origin) : Immovable(origin, 1, 1, ['#']) { }
+
+    private class Empty(Vector2Int origin) : GameObject(origin, 1, 1, ['.'])
+    {
         public override bool CanPush(Vector2Int dir, GameObject[,] map) => true;
 
         public override void Push(Vector2Int dir, GameObject[,] map) { }
     }
 
-    private class Box : GameObject
-    {
-        public Box(int x, int y)
-        {
-            StartX = x;
-            StartY = y;
-            EndX = x;
-            EndY = y;
-            Type = 'O';
-        }
+    private class Box(Vector2Int origin) : Pushable(origin, 1, 1, ['O']) { }
 
-        public override bool CanPush(Vector2Int dir, GameObject[,] map)
-        {
-            var nextX = EndX + dir.X;
-            var nextY = EndY + dir.Y;
-            var next = map[nextX, nextY];
-            return next.CanPush(dir, map);
-        }
+    private class DoubleBox(Vector2Int origin) : Pushable(origin, 2, 1, ['[', ']']) { }
 
-        public override void Push(Vector2Int dir, GameObject[,] map)
-        {
-            var nextX = EndX + dir.X;
-            var nextY = EndY + dir.Y;
-            var next = map[nextX, nextY];
-            next.Push(dir, map);
-            EndX += dir.X;
-            EndY += dir.Y;
-            map[EndX, EndY] = this;
-            map[StartX, StartY] = new Empty(StartX, StartY);
-        }
-    }
-
-    private class Player : GameObject
-    {
-        public Player(int x, int y)
-        {
-            StartX = x;
-            StartY = y;
-            EndX = x;
-            EndY = y;
-            Type = '@';
-        }
-
-        public override bool CanPush(Vector2Int dir, GameObject[,] map)
-        {
-            var nextX = EndX + dir.X;
-            var nextY = EndY + dir.Y;
-            var next = map[nextX, nextY];
-            return next.CanPush(dir, map);
-        }
-
-        public override void Push(Vector2Int dir, GameObject[,] map)
-        {
-            var nextX = EndX + dir.X;
-            var nextY = EndY + dir.Y;
-            var next = map[nextX, nextY];
-            next.Push(dir, map);
-            EndX += dir.X;
-            EndY += dir.Y;
-            map[EndX, EndY] = this;
-            map[StartX, StartY] = new Empty(StartX, StartY);
-        }
-    }
+    private class Player(Vector2Int origin) : Pushable(origin, 1, 1, ['@']) { }
 
     private static Vector2Int CharToDir(char c) =>
         c switch
@@ -154,92 +166,116 @@ public partial class WarehouseWoes : AdventOfCodeSolution<long>
         var rawMap = chunked.First().Select(x => x.ToCharArray()).ToArray();
         var movements = string.Join("", chunked.Skip(1).SelectMany(x => x)).ToCharArray();
 
-        MapTile[,] map = new MapTile[rawMap.Length, rawMap[0].Length];
-        var player = Vector2Int.Zero;
         int n = rawMap.Length,
             m = rawMap[0].Length;
 
-        for (int i = 0; i < n; i++)
+        var map = new GameObject[n, m];
+        Player player = new(Vector2Int.Zero);
+
+        for (int y = 0; y < n; y++)
         {
-            for (int j = 0; j < m; j++)
+            for (int x = 0; x < m; x++)
             {
-                map[i, j] = (MapTile)rawMap[i][j];
-                if (map[i, j] == MapTile.Player)
+                var c = rawMap[y][x];
+                var origin = new Vector2Int(x, y);
+                map[y, x] = c switch
                 {
-                    player = new Vector2Int(i, j);
+                    '#' => new Wall(origin),
+                    '.' => new Empty(origin),
+                    'O' => new Box(origin),
+                    '@' => new Player(origin),
+                    _ => throw new InvalidOperationException(),
+                };
+
+                if (map[y, x] is Player player1)
+                {
+                    player = player1;
                 }
             }
         }
 
+        return Play(player, map, movements);
+    }
+
+    protected override long PartTwo(string[] lines)
+    {
+        var chunked = lines.PartitionBy(string.IsNullOrWhiteSpace);
+        var rawMap = chunked.First().Select(x => x.ToCharArray()).ToArray();
+        var movements = string.Join("", chunked.Skip(1).SelectMany(x => x)).ToCharArray();
+
+        int n = rawMap.Length,
+            origM = rawMap[0].Length;
+
+        var map = new GameObject[n, (origM * 2)];
+        Player player = new(Vector2Int.Zero);
+
+        for (int y = 0; y < n; y++)
+        {
+            var newX = 0;
+            for (int x = 0; x < origM; x++)
+            {
+                var c = rawMap[y][x];
+                var origin = new Vector2Int(newX, y);
+                var nextOrigin = origin + Vector2Int.Right;
+
+                if (c == '@')
+                {
+                    player = new Player(origin);
+                    map[y, newX] = player;
+                    map[y, newX + 1] = new Empty(nextOrigin);
+                }
+                else if (c == 'O')
+                {
+                    var doubleBox = new DoubleBox(origin);
+                    map[y, newX] = doubleBox;
+                    map[y, newX + 1] = doubleBox;
+                }
+                else if (c == '#')
+                {
+                    map[y, newX] = new Wall(origin);
+                    map[y, newX + 1] = new Wall(nextOrigin);
+                }
+                else if (c == '.')
+                {
+                    map[y, newX] = new Empty(origin);
+                    map[y, newX + 1] = new Empty(nextOrigin);
+                }
+
+                newX += 2;
+            }
+        }
+
+        return Play(player, map, movements);
+    }
+
+    private static long Play(Player player, GameObject[,] map, char[] movements)
+    {
         foreach (var movement in movements)
         {
             var dir = CharToDir(movement);
-            player = Push(player, map, dir);
+            if (player.CanPush(dir, map))
+            {
+                player.Push(dir, map);
+            }
         }
 
         long result = 0;
 
-        for (int i = 0; i < n; i++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            for (int j = 0; j < m; j++)
+            for (int x = 0; x < map.GetLength(1); x++)
             {
-                if (map[i, j] != MapTile.Box)
+                if (map[y, x].Origin != new Vector2Int(x, y))
                 {
                     continue;
                 }
-
-                result += 100 * i + j;
+                if (map[y, x] is Box or DoubleBox)
+                {
+                    result += y * 100 + x;
+                }
             }
         }
 
         return result;
     }
-
-    protected override long PartTwo(string[] lines)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static Vector2Int Push(Vector2Int pos, MapTile[,] map, Vector2Int dir)
-    {
-        var next = pos + dir;
-        if (map[next.Y, next.X] == MapTile.Edge)
-        {
-            return pos;
-        }
-
-        if (map[next.Y, next.X] == MapTile.Empty)
-        {
-            map[next.Y, next.X] = MapTile.Player;
-            map[pos.Y, pos.X] = MapTile.Empty;
-            return next;
-        }
-
-        var toPush = new List<Vector2Int>();
-
-        while (map[next.Y, next.X] != MapTile.Edge && map[next.Y, next.X] != MapTile.Empty)
-        {
-            toPush.Add(next);
-            next += dir;
-        }
-
-        if (map[next.Y, next.X] == MapTile.Edge)
-        {
-            return pos;
-        }
-
-        map[pos.Y, pos.X] = MapTile.Empty;
-
-        foreach (var p in toPush)
-        {
-            var nextPos = p + dir;
-            map[nextPos.Y, nextPos.X] = MapTile.Box;
-        }
-
-        next = pos + dir;
-        map[next.Y, next.X] = MapTile.Player;
-        return next;
-    }
-
-    // PART TWO - this one sucks, I won't reimplement part one to use the same logic
 }
